37d0addaa30babc0996dc8b7a9bc10a9903d8dfe
mounir <mounirghafirlw@gmail.com>
2026-04-30 09:52:42 +0100

    Foundation & Global Styles

---
+++ src/constants/initialData.js
@@ -0,0 +1,53 @@
+export const MOVIE_GENRES = [
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Drama",
+  "Fantasy",
+  "Horror",
+  "Mystery",
+  "Romance",
+  "Sci-Fi",
+  "Thriller",
+  "Animation",
+  "Documentary"
+];
+
+export const INITIAL_MOVIES = [
+  {
+    id: "1",
+    title: "Inception",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    year: 2010,
+    genre: "Sci-Fi",
+    director: "Christopher Nolan",
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+    image: "https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?q=80&w=2070&auto=format&fit=crop",
+    trailer: "https://www.youtube.com/watch?v=YoHD9XEInc0",
+    rating: 5
+  },
+  {
+    id: "2",
+    title: "The Dark Knight",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    year: 2008,
+    genre: "Action",
+    director: "Christopher Nolan",
+    actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    image: "https://images.unsplash.com/photo-1478720568477-152d9b164e26?q=80&w=2070&auto=format&fit=crop",
+    trailer: "https://www.youtube.com/watch?v=EXeTwQWaywY",
+    rating: 5
+  },
+  {
+    id: "3",
+    title: "Interstellar",
+    description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+    year: 2014,
+    genre: "Sci-Fi",
+    director: "Christopher Nolan",
+    actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+    image: "https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?q=80&w=2072&auto=format&fit=crop",
+    trailer: "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+    rating: 4
+  }
+];
